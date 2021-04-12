4c488911ec9199755bba37d08d986bdb61f027e2
valeriofranchi <v.franchi@hotmail.com>
2021-04-12 12:02:30 +0100

    fixed issue in dead reckoning

--- assignment2.ino
+++ assignment2.ino
@@ -13,11 +13,11 @@
 #define MAX_ROLLING_AVERAGE            (MAX_SPEED * MAX_ROLLING_AVERAGE_PERCENT / 100)
 #define NUMBER_MEASUREMENTS_AVERAGE    6
 
-#define DECREASE_RATE   20
-#define INCREASE_RATE   20
+#define DECREASE_RATE       20
+#define INCREASE_RATE       20
 
-#define SPEED_INCREASE  MAX_SPEED * INCREASE_RATE / 100
-#define SPEED_DECREASE  MAX_SPEED * DECREASE_RATE / 100
+#define SPEED_INCREASE      MAX_SPEED * INCREASE_RATE / 100
+#define SPEED_DECREASE      MAX_SPEED * DECREASE_RATE / 100
 
 #define MONITOR_FREQ        2
 #define FLASH_FREQ          4 
@@ -27,14 +27,14 @@
 #define DISTANCE_BETWEEN_WHEEL_CENTRES  2
 #define ODOMETRY_UPDATE_TIME            1
 
-#define TO_PWM(x)       map((x), -MAX_SPEED, MAX_SPEED, -PWM_MAX_SPEED, PWM_MAX_SPEED)
-#define TO_RPM(x)       map((x), -PWM_MAX_SPEED, PWM_MAX_SPEED, -MAX_SPEED, MAX_SPEED)
+#define TO_PWM(x)         map((x), -MAX_SPEED, MAX_SPEED, -PWM_MAX_SPEED, PWM_MAX_SPEED)
+#define TO_RPM(x)         map((x), -PWM_MAX_SPEED, PWM_MAX_SPEED, -MAX_SPEED, MAX_SPEED)
 
-#define DEBOUNCE_DELAY   500
+#define DEBOUNCE_DELAY    500
 
-#define DIAGNOSTIC_PERIOD         2000UL
-#define SENSOR_PROCESSING_PERIOD  20UL
-#define CPU_BUSY_PERIOD           5UL
+#define DIAGNOSTIC_PERIOD           2000UL
+#define SENSOR_PROCESSING_PERIOD    20UL
+#define CPU_BUSY_PERIOD             5UL
 
 // buttons on Arduino 
 #define INCREASE_LEFT_BUTTON    10
@@ -88,19 +88,20 @@ static void TaskProcessing ( void *pvParameters);
 static void TaskMonitor (void *pvParameters );
 static void TaskDeadReckoning (void *pvParameters );
 
-// task handle
+// sensor processing task handle, timer handle and timer callback
 static TaskHandle_t xTaskHandle;
-
 static TimerHandle_t xProcessingTimer;
-
 static void vProcessingTimerCallback( TimerHandle_t xTimer );
 
-// interrupt handler function
+// interrupt handler function 
 void breakInterruptHandler();
 void diagnosticInterruptHandler();
+
+// debounce time for diagnostic  
 volatile unsigned long lastDebounceDiagnostic = 0;
 
 void setup() {
+  // set strings for logging 
   outputs[0] = "v: "; outputs[1] = "omega: "; outputs[2] = "x: ";
   outputs[3] = "y: "; outputs[4] = "theta: ";                          
     
@@ -124,6 +125,7 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(BRAKE_BUTTON), breakInterruptHandler, FALLING);
   attachInterrupt(digitalPinToInterrupt(DIAGNOSTIC_MODE_BUTTON), diagnosticInterruptHandler, LOW);
 
+  // create timer 
   xProcessingTimer = xTimerCreate(   "Processing timer",
                                       SENSOR_PROCESSING_PERIOD,   
                                       pdFALSE,              
@@ -178,7 +180,6 @@ void setup() {
 
   // start scheduler so created task start executing 
   vTaskStartScheduler();
-
 }
 
 void loop() {  
@@ -214,7 +215,7 @@ void TaskUpdateMotorSpeeds( void *pvParameters) {
       }
       
       // if last debounce time is more than a threshold, enter loop        
-      if ( (millis() - lastDebounceDecreaseLeft > DEBOUNCE_DELAY) && (decreaseLeft == LOW) ) {
+      if ( (TIME_MS - lastDebounceDecreaseLeft > DEBOUNCE_DELAY) && (decreaseLeft == LOW) ) {
         // if button is pressed and break is not activated decrease speed
         left -= SPEED_DECREASE;
         if (left < -MAX_SPEED) 
@@ -268,19 +269,19 @@ void TaskUpdateMotorSpeeds( void *pvParameters) {
 static void TaskDeadReckoning( void * pvParameters ) {
   (void) pvParameters;
 
-  long lastUpdateTime = millis();
-  long lastPrintTime = millis();
+  long lastUpdateTime = TIME_MS;
+  long lastPrintTime = TIME_MS;
 
-  long dt = 0;
+  float dt = 0;
   float R = 0; float omega = 0;
   float ICCx = 0; float ICCy = 0;
 
   for (;;) {
 
     // print odometry every second 
-    if ( (millis() - lastPrintTime) > ODOMETRY_UPDATE_TIME * 1000 ) {
-      //logOdometry();
-      lastPrintTime = millis();
+    if ( (TIME_MS - lastPrintTime) > ODOMETRY_UPDATE_TIME * 1000 ) {
+      logOdometry();
+      lastPrintTime = TIME_MS;
 /*
       unsigned temp;
       temp = uxTaskGetStackHighWaterMark(NULL);
@@ -293,19 +294,20 @@ static void TaskDeadReckoning( void * pvParameters ) {
       */
     }
     
+    // update change in time 
+    dt = (TIME_MS - lastUpdateTime) / 1000.0;
+    
     // no rotation, movement in straight line -> limiting case, else R -> infinity 
-    dt = (millis() - lastUpdateTime) / 1000;
     if (left == right) {      
-      lastUpdateTime = millis();
+      lastUpdateTime = TIME_MS;
       
       pose.x += right * cos(pose.theta) * dt;
       pose.y += right * sin(pose.theta) * dt;
 
       pose.linearVel = right;
       pose.rotVel = 0.0;
-    }
-    else {           
-      lastUpdateTime = millis();
+    } else {     
+      lastUpdateTime = TIME_MS;
       R = (DISTANCE_BETWEEN_WHEEL_CENTRES / 2) * ( (left + right) / (right - left) );
       omega = (right - left) / DISTANCE_BETWEEN_WHEEL_CENTRES;
   
@@ -332,12 +334,12 @@ static void TaskDiagnostic( void *pvParameters ) {
     uint16_t currentLeft = 0; uint16_t currentRight = 0;
     uint16_t absoluteLeft = 0; uint16_t absoluteRight = 0;   
 
-    long start = millis();
+    long start = TIME_MS;
     
     for (;;) {
       if (diagnosticState == 1) {
 
-        if ( (millis() - start) < DIAGNOSTIC_PERIOD) {
+        if ( (TIME_MS - start) < DIAGNOSTIC_PERIOD) {
 
           // check currents in DC motors
           int senseLeft = analogRead(SENSE_LEFT);
@@ -370,9 +372,10 @@ static void TaskDiagnostic( void *pvParameters ) {
           Serial.println(absoluteRight);
 
           // reset current values 
-          currentRight = 0; currentLeft = 0; 
+          currentRight = 0; 
+          currentLeft = 0; 
           
-          start = millis();
+          start = TIME_MS;
 /*
       unsigned temp;
       temp = uxTaskGetStackHighWaterMark(NULL);
@@ -396,14 +399,17 @@ static void TaskProcessing ( void *pvParameters) {
   (void) pvParameters;
   
   for (;;) {
-    long initialTime = TIME_MS;
+    // stop timer
     xTimerStop( xProcessingTimer, 0);
-    while ((TIME_MS - initialTime) < CPU_BUSY_PERIOD) {
+
+    // keep CPU busy for 5ms 
+    long initialTime = TIME_MS;
+    while ((TIME_MS - initialTime) < CPU_BUSY_PERIOD) 
       ;
-    }
-    
+
+    // start timer 
     xTimerStart( xProcessingTimer, 0);
-    
+    /*
      unsigned temp;
       temp = uxTaskGetStackHighWaterMark(NULL);
     
@@ -411,12 +417,15 @@ static void TaskProcessing ( void *pvParameters) {
         stack_hwm = temp;
         Serial.print(F(", High Watermark from function1: "));
         Serial.println(stack_hwm); // https://www.freertos.org/uxTaskGetStackHighWaterMark.html
-      }
+      }*/
+
+    // set priority to 1 
     vTaskPrioritySet(NULL, 1);
   }  
 }
 
 static void vProcessingTimerCallback( TimerHandle_t xTimer ) {
+  // reset priority of sensor processing to 2 after 15ms 
   static long start = TIME_MS;
   if ( (TIME_MS - start) > (SENSOR_PROCESSING_PERIOD - CPU_BUSY_PERIOD)) 
     vTaskPrioritySet(xTaskHandle, 2);  
@@ -437,13 +446,13 @@ static void TaskMonitor( void *pvParameters) {
     measurementsRight[i] = 0;
   }
   
-  long lastAverageUpdate = millis();
-  long lastFlash = millis();
+  long lastAverageUpdate = TIME_MS;
+  long lastFlash = TIME_MS;
   int flashCount = 0;
   
   for (;;) {
 
-    if ( ((millis() - lastAverageUpdate) > ((1 / MONITOR_FREQ) * 1000)) && diagnosticState == 0) {
+    if ( ((TIME_MS - lastAverageUpdate) > ((1 / MONITOR_FREQ) * 1000)) && diagnosticState == 0) {
       
       // remove pointer element from sum
       sumLeft -= TO_RPM(measurementsLeft[curr]);
@@ -465,7 +474,7 @@ static void TaskMonitor( void *pvParameters) {
       // check if rolling average is higher than maximum
       if ((sumLeft / NUMBER_MEASUREMENTS_AVERAGE) > MAX_ROLLING_AVERAGE && ((sumRight / NUMBER_MEASUREMENTS_AVERAGE) > MAX_ROLLING_AVERAGE)) {
           
-        if ( ((millis() - lastFlash) > ((1 / FLASH_FREQ) * 1000)) && (flashCount < NUM_FLASHES)) {
+        if ( ((TIME_MS - lastFlash) > ((1 / FLASH_FREQ) * 1000)) && (flashCount < NUM_FLASHES)) {
            // blink LED
           digitalWrite(MONITOR_LED, HIGH);
           digitalWrite(MONITOR_LED, LOW);
@@ -476,14 +485,12 @@ static void TaskMonitor( void *pvParameters) {
             flashCount = 0;
     
           // update time of last blink 
-          lastFlash = millis();
-    
-          
+          lastFlash = TIME_MS;
        }
      } else {
       flashCount = 0;
      }      
-      lastAverageUpdate = millis();   
+      lastAverageUpdate = TIME_MS;   
     }
     /*
     unsigned temp;
@@ -508,7 +515,7 @@ void breakInterruptHandler() {
     left = 0;
     right = 0;
     changeMotorSpeeds(left, right, 1);
-    logOdometry();
+    //logOdometry();
     
     brakeState = 1;
 
@@ -528,10 +535,10 @@ void breakInterruptHandler() {
 
 void diagnosticInterruptHandler() {
   // if last debounce time is more than a threshold, modify diagnostic state 
-  if ( (millis() - lastDebounceDiagnostic) > DEBOUNCE_DELAY ) {
+  if ( (TIME_MS - lastDebounceDiagnostic) > DEBOUNCE_DELAY ) {
     diagnosticState = !diagnosticState;
     
-    lastDebounceDiagnostic = millis();     
+    lastDebounceDiagnostic = TIME_MS;     
   }
 }
 
